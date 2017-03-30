d1fb211f223d259c993a244e155213c5da30eda1
Fabian Homborg <FHomborg@gmail.com>
2017-03-30 21:54:05 +0200

    First step in implementing meson
    A build system written in python using ninja.
    
    Currently fails on csignal.

---
+++ meson_config.h
@@ -0,0 +1,288 @@
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* HACK: These are provided by meson */
+#mesondefine BINDIR
+#mesondefine DATADIR
+#mesondefine DOCDIR
+#mesondefine SYSCONFDIR
+
+/* Define to 1 if you have the `backtrace_symbols' function. */
+#mesondefine HAVE_BACKTRACE_SYMBOLS
+
+/* Define to 1 if you have the `clock_gettime' function. */
+#mesondefine HAVE_CLOCK_GETTIME
+
+/* Define to 1 if you have the <curses.h> header file. */
+#mesondefine HAVE_CURSES_H
+
+/* define if the compiler supports basic C++11 syntax */
+#mesondefine HAVE_CXX11
+
+/* Define to 1 if you have the <dirent.h> header file, and it defines `DIR'.
+   */
+#mesondefine HAVE_DIRENT_H
+
+/* Define to 1 if you have the `dirfd' function. */
+#mesondefine HAVE_DIRFD
+
+/* Define to 1 if you have the <execinfo.h> header file. */
+#mesondefine HAVE_EXECINFO_H
+
+/* Define to 1 if you have the `flock' function. */
+#mesondefine HAVE_FLOCK
+
+/* Define to 1 if you have the `futimens' function. */
+#mesondefine HAVE_FUTIMENS
+
+/* Define to 1 if you have the `futimes' function. */
+#mesondefine HAVE_FUTIMES
+
+/* Define to 1 if you have the `getifaddrs' function. */
+#mesondefine HAVE_GETIFADDRS
+
+/* Define to 1 if you have the <getopt.h> header file. */
+#mesondefine HAVE_GETOPT_H
+
+/* Define to 1 if you have the `getpwent' function. */
+#mesondefine HAVE_GETPWENT
+
+/* Define to 1 if you have the `gettext' function. */
+#mesondefine HAVE_GETTEXT
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#mesondefine HAVE_INTTYPES_H
+
+/* Define to 1 if you have the `killpg' function. */
+#mesondefine HAVE_KILLPG
+
+/* Define to 1 if you have the <libintl.h> header file. */
+#mesondefine HAVE_LIBINTL_H
+
+/* Define to 1 if you have the `lrand48_r' function. */
+#mesondefine HAVE_LRAND48_R
+
+/* Define to 1 if you have the <memory.h> header file. */
+#mesondefine HAVE_MEMORY_H
+
+/* Define to 1 if you have the `mkostemp' function. */
+#mesondefine HAVE_MKOSTEMP
+
+/* Define to 1 if you have the <ncurses/curses.h> header file. */
+#mesondefine HAVE_NCURSES_CURSES_H
+
+/* Define to 1 if you have the <ncurses.h> header file. */
+#mesondefine HAVE_NCURSES_H
+
+/* Define to 1 if you have the <ncurses/term.h> header file. */
+#mesondefine HAVE_NCURSES_TERM_H
+
+/* Define to 1 if you have the <ndir.h> header file, and it defines `DIR'. */
+#mesondefine HAVE_NDIR_H
+
+/* Define to 1 if the shm_open() function exists */
+#mesondefine HAVE_SHM_OPEN
+
+/* Define to 1 if you have the <siginfo.h> header file. */
+#mesondefine HAVE_SIGINFO_H
+
+/* Define to 1 if you have the <spawn.h> header file. */
+#mesondefine HAVE_SPAWN_H
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#mesondefine HAVE_STDINT_H
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#mesondefine HAVE_STDLIB_H
+
+/* Define to 1 if you have the `std::wcscasecmp' function. */
+#mesondefine HAVE_STD__WCSCASECMP
+
+/* Define to 1 if you have the `std::wcsdup' function. */
+#mesondefine HAVE_STD__WCSDUP
+
+/* Define to 1 if you have the `std::wcsncasecmp' function. */
+#mesondefine HAVE_STD__WCSNCASECMP
+
+/* Define to 1 if you have the <strings.h> header file. */
+#mesondefine HAVE_STRINGS_H
+
+/* Define to 1 if you have the <string.h> header file. */
+#mesondefine HAVE_STRING_H
+
+/* Define to 1 if you have the <stropts.h> header file. */
+#mesondefine HAVE_STROPTS_H
+
+/* Define to 1 if `d_type' is a member of `struct dirent'. */
+#mesondefine HAVE_STRUCT_DIRENT_D_TYPE
+
+/* Define to 1 if `st_ctime_nsec' is a member of `struct stat'. */
+#mesondefine HAVE_STRUCT_STAT_ST_CTIME_NSEC
+
+/* Define to 1 if `st_mtimespec.tv_nsec' is a member of `struct stat'. */
+#mesondefine HAVE_STRUCT_STAT_ST_MTIMESPEC_TV_NSEC
+
+/* Define to 1 if `st_mtim.tv_nsec' is a member of `struct stat'. */
+#mesondefine HAVE_STRUCT_STAT_ST_MTIM_TV_NSEC
+
+/* Define to 1 if you have the <sys/dir.h> header file, and it defines `DIR'.
+   */
+#mesondefine HAVE_SYS_DIR_H
+
+/* Define to 1 if the sys_errlist array is available. */
+#mesondefine HAVE_SYS_ERRLIST
+
+/* Define to 1 if you have the <sys/ioctl.h> header file. */
+#mesondefine HAVE_SYS_IOCTL_H
+
+/* Define to 1 if you have the <sys/ndir.h> header file, and it defines `DIR'.
+   */
+#mesondefine HAVE_SYS_NDIR_H
+
+/* Define to 1 if you have the <sys/resource.h> header file. */
+#mesondefine HAVE_SYS_RESOURCE_H
+
+/* Define to 1 if you have the <sys/select.h> header file. */
+#mesondefine HAVE_SYS_SELECT_H
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#mesondefine HAVE_SYS_STAT_H
+
+/* Define to 1 if you have the <sys/sysctl.h> header file. */
+#mesondefine HAVE_SYS_SYSCTL_H
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#mesondefine HAVE_SYS_TYPES_H
+
+/* Define to 1 if you have the <termios.h> header file. */
+#mesondefine HAVE_TERMIOS_H
+
+/* Define to 1 if you have the <term.h> header file. */
+#mesondefine HAVE_TERM_H
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#mesondefine HAVE_UNISTD_H
+
+/* Define to 1 if you have the `wcscasecmp' function. */
+#mesondefine HAVE_WCSCASECMP
+
+/* Define to 1 if you have the `wcsdup' function. */
+#mesondefine HAVE_WCSDUP
+
+/* Define to 1 if you have the `wcslcpy' function. */
+#mesondefine HAVE_WCSLCPY
+
+/* Define to 1 if you have the `wcsncasecmp' function. */
+#mesondefine HAVE_WCSNCASECMP
+
+/* Define to 1 if you have the `wcsndup' function. */
+#mesondefine HAVE_WCSNDUP
+
+/* Define to 1 if the winsize struct and TIOCGWINSZ macro exist */
+#mesondefine HAVE_WINSIZE
+
+/* Define to 1 if the _nl_msg_cat_cntr symbol is exported. */
+#mesondefine HAVE__NL_MSG_CAT_CNTR
+
+/* Define to 1 if you have the file `/proc/self/stat'. */
+#mesondefine HAVE__PROC_SELF_STAT
+
+/* Define to 1 if the _sys_errs array is available. */
+#mesondefine HAVE__SYS__ERRS
+
+/* Define to 1 to disable ncurses macros that conflict with the STL */
+#mesondefine NCURSES_NOMACROS
+
+/* Define to 1 to disable curses macros that conflict with the STL */
+#mesondefine NOMACROS
+
+/* Define to the address where bug reports for this package should be sent. */
+#mesondefine PACKAGE_BUGREPORT
+
+/* Define to the full name of this package. */
+#mesondefine PACKAGE_NAME
+
+/* Define to the full name and version of this package. */
+#mesondefine PACKAGE_STRING
+
+/* Define to the one symbol short name of this package. */
+#mesondefine PACKAGE_TARNAME
+
+/* Define to the home page for this package. */
+#mesondefine PACKAGE_URL
+
+/* Define to the version of this package. */
+#mesondefine PACKAGE_VERSION
+
+/* The size of `wchar_t', as computed by sizeof. */
+#mesondefine SIZEOF_WCHAR_T
+
+/* Define to 1 if you have the ANSI C header files. */
+#mesondefine STDC_HEADERS
+
+/* Define to 1 if tparm accepts a fixed amount of paramters. */
+#mesondefine TPARM_SOLARIS_KLUDGE
+
+/* Perform string translations with gettext */
+#mesondefine USE_GETTEXT
+
+/* Enable extensions on AIX 3, Interix.  */
+#ifndef _ALL_SOURCE
+# define _ALL_SOURCE 1
+#endif
+/* Enable GNU extensions on systems that have them.  */
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+/* Enable threading extensions on Solaris.  */
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+/* Enable extensions on HP NonStop.  */
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+/* Enable general extensions on Solaris.  */
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+
+
+/* The size of wchar_t in bits. */
+#mesondefine WCHAR_T_BITS
+
+/* Enable large inode numbers on Mac OS X 10.5.  */
+#ifndef _DARWIN_USE_64_BIT_INODE
+# define _DARWIN_USE_64_BIT_INODE 1
+#endif
+
+/* Number of bits in a file offset, on hosts where this is settable. */
+#mesondefine _FILE_OFFSET_BITS
+
+/* Define for large files, on AIX-style hosts. */
+#mesondefine _LARGE_FILES
+
+/* Define to 1 if on MINIX. */
+#mesondefine _MINIX
+
+/* Define to 2 if the system does not provide POSIX.1 features except with
+   this defined. */
+#mesondefine _POSIX_1_SOURCE
+
+/* Define to 1 if you need to in order for `stat' and other things to work. */
+#mesondefine _POSIX_SOURCE
+
+#if __GNUC__ >= 3
+#ifndef __warn_unused
+#define __warn_unused __attribute__ ((warn_unused_result))
+#endif
+#ifndef __sentinel
+#define __sentinel __attribute__ ((sentinel))
+#endif
+#ifndef __packed
+#define __packed __attribute__ ((packed))
+#endif
+#else
+#define __warn_unused
+#define __sentinel
+#define __packed
+#endif
