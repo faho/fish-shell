4534c973cf0f117b884e7caf136c5a1d92d89683
Fabian Homborg <FHomborg@gmail.com>
2018-12-11 20:32:55 +0100

    Try defining TPARM_VARARGS earlier

--- src/fallback.h
+++ src/fallback.h
@@ -63,9 +63,7 @@ struct winsize {
 
 #endif
 
-#ifdef __NetBSD__
-#define TPARM_VARARGS 1
-#elif defined(TPARM_SOLARIS_KLUDGE)
+#if defined(TPARM_SOLARIS_KLUDGE)
 /// Solaris tparm has a set fixed of paramters in its curses implementation, work around this here.
 #define tparm tparm_solaris_kludge
 char *tparm_solaris_kludge(char *str, long p1 = 0, long p2 = 0, long p3 = 0, long p4 = 0,
