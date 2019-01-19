44a4dd836fa2cd52baf443ca437934339daea75a
Fabian Homborg <FHomborg@gmail.com>
2019-01-19 13:08:46 +0100

    WIP Skip check on glibc

--- test_wcwidth.cpp
+++ test_wcwidth.cpp
@@ -1,10 +1,25 @@
 #include <wchar.h>
 #include <iostream>
 
-int main(int argc, char** argv) {
-    // This initializes the locale according to the environment variables.
-    // That means if the environment isn't set up for unicode, this whole excercise is pointless.
+int main() {
+    // Check wcwidth of a character that is wide in Unicode 9,
+    // but narrow before.
+
+    // For glibc, we skip the check,
+    // because we know that >= 2.26 can do it and < can not.
+    // This allows us to not depend on the locale.
+#if defined(__GLIBC_PREREQ)
+#if __GLIBC_PREREQ(2, 26)
+    std::cout << 2;
+# else
+    std::cout << 1;
+#endif
+#else
+    // We're not on glibc, so we actually perform the check.
+    // This unfortunately requires a multibyte-locale.
     setlocale(LC_ALL, "");
+    // This character picked somewhat arbitrary.
     std::cout << wcwidth(L'😃');
+#endif
     return 0;
 }
